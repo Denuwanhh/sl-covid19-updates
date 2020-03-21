24daf327310682a362ef8dc268c0bfc479021de6
Denuwan Himanga Hettiarachi <denuwanonline@gmail.com>
2020-03-21 17:49:22 +0530

    Fixed minor bug, add loading bar

--- popup.js
+++ popup.js
@@ -4,6 +4,11 @@ x.open('GET', 'http://hpb.health.gov.lk/api/get-current-statistical');
 x.onload = function() {   
     var statisticalData = JSON.parse(x.responseText);
 
+    document.getElementById('body-con-id').innerHTML = "<canvas id='main-pie-chart' class='default-padding' width='100' height='50'></canvas>"
+                                                              +"<div class='topic-data-div' id='td-data-header-id'></div>"
+                                                              +"<div id='sub-data-grid'></div>" 
+                                                              +"<div class='footer' id='footer-id'></div>";
+
     var ctx = document.getElementById('main-pie-chart').getContext('2d');
 
     var mainDataPieChart = new Chart(ctx, {
@@ -30,7 +35,16 @@ x.onload = function() {
         }
     });
 
-    document.getElementById('main-sum').innerHTML = "<td class='td-data td-tot' >"+statisticalData.data.local_total_cases+"</td> <td class='td-data td-rec'>"+statisticalData.data.local_recovered+"</td> <td class='td-data td-death'>"+statisticalData.data.local_new_deaths+"</td>";
+    document.getElementById('td-data-header-id').innerHTML = "<table style='width:100%; border-spacing: 0px '>"
+                                                                +"<tr>"
+                                                                +"<td class='td-data-header td-tot' title='Total local confirmed COVID-19 cases reported in Sri Lanka'>Total</td>"
+                                                                +"<td class='td-data-header td-rec' title='Total local COVID-19 cases recovered and discharged in Sri Lanka'>Recovered</td>"
+                                                                +"<td class='td-data-header td-death' title='Total deaths due to COVID-19 reported in Sri Lanka'>Death</td>"
+                                                                +"</tr>"
+                                                                +"<tr  id='main-sum'></tr>"
+                                                                +"</table>";
+
+    document.getElementById('main-sum').innerHTML = "<td class='td-data td-tot' title='Total local confirmed COVID-19 cases reported in Sri Lanka' >"+statisticalData.data.local_total_cases+"</td> <td class='td-data td-rec' title='Total local COVID-19 cases recovered and discharged in Sri Lanka'>"+statisticalData.data.local_recovered+"</td> <td class='td-data td-death' title='Total deaths due to COVID-19 reported in Sri Lanka'>"+statisticalData.data.local_new_deaths+"</td>";
 
     var i;
     var subDataDiv = "";
@@ -53,8 +67,9 @@ x.onload = function() {
 
     document.getElementById('sub-data-grid').innerHTML = subDataDiv;
 
-    document.getElementById('update-on').innerHTML = statisticalData.data.update_date_time;
+    document.getElementById('update-on').innerHTML = "Last Updated On: "+statisticalData.data.update_date_time;
 
+    document.getElementById('footer-id').innerHTML = "Data Source : Health Promotion Bureau, Sri Lanka";
 
 };
 
