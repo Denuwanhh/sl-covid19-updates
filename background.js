7ae477eda77efd04f7e7cb1a290854e3ff0d5168
Denuwan Himanga Hettiarachi <denuwanonline@gmail.com>
2021-08-23 01:23:52 +0530

    Add Google news RSS feeds to get local news

--- background.js
+++ background.js
@@ -9,36 +9,56 @@
  	});
 
  });
+ 
 
  chrome.alarms.onAlarm.addListener(function( alarm ) {
 
-  var x = new XMLHttpRequest();
+  const news_sources = ["www.newsfirst.lk", "www.adaderana.lk", "www.newswire.lk", "www.dailymirror.lk", "www.colombopage.com", "www.dailynews.lk"];
+  
+  for (let i = 0; i < news_sources.length; i++) {
 
-  x.open('GET', 'http://hpb.health.gov.lk/api/get-current-statistical');
-  x.onload = function() {   
-  	var statisticalData = JSON.parse(x.responseText);
+	setTimeout(function () {
+		const RSS_URL = 'https://news.google.com/rss/search?q=Covid 19 site:' + news_sources[i] + '&hl=en-US&gl=US&ceid=US:en';	
 
-  	chrome.storage.sync.get('updatetime', function(data) {
-  		var time = data == null ? null : data.updatetime;
+		var x = new XMLHttpRequest();
+		x.open('GET', RSS_URL);
+		x.onload = function() {
+			
+			var parser = new DOMParser();
+			var xmlDoc = parser.parseFromString(x.responseText,"text/xml");
+	  
+			chrome.storage.sync.get('updatetime', function(data) {
+				
+				var time = (data === null || (typeof data.updatetime === 'undefined')) ? 0 : data.updatetime;
+				var new_time = Date.parse(xmlDoc.getElementsByTagNameNS('*','pubDate')[0].childNodes[0].nodeValue);
+	  
+				console.log(news_sources[i])
+				console.log("Last updated on : "+time.toString()+", Source Last updated time : "+new_time);
+	  
+				if(time < new_time){
+					
+					chrome.storage.sync.set({updatetime: new_time}, function() { 
+						console.log("Last updates time Set : " + new_time);
+					});
 
-  		console.log("Last updated on : "+time+", HPB Last updated time : "+statisticalData.data.update_date_time);
-
-  		if(time != statisticalData.data.update_date_time){
-
-  			chrome.storage.sync.set({updatetime: statisticalData.data.update_date_time}, function() { 
-  				console.log("Last updates time Set : " + statisticalData.data.update_date_time);
-  			});
-
-  			chrome.notifications.create('', {
-  				title: 'Sri Lanka COVID-19 Updates',
-  				message: statisticalData.data.local_new_cases + ' new cases found during the last 24 hours. '+ statisticalData.data.local_total_cases  +' total confirmed cases reported.',
-  				iconUrl: 'images/logo128.png',
-  				type: 'basic'
-  			});
-  		}
-
-  	});
-  };
-
-  x.send();
+					chrome.storage.sync.set({latest_news: xmlDoc.getElementsByTagNameNS('*','title')[1].childNodes[0].nodeValue, latest_news_link : xmlDoc.getElementsByTagNameNS('*','link')[1].childNodes[0].nodeValue}, function() { 
+						console.log("Latest news Set : " + xmlDoc.getElementsByTagNameNS('*','title')[1].childNodes[0].nodeValue);
+						console.log("Latest news link Set : " + xmlDoc.getElementsByTagNameNS('*','link')[1].childNodes[0].nodeValue);
+					});
+	  
+					chrome.notifications.create('', {
+						title: 'Sri Lanka COVID-19 Updates',
+						message: xmlDoc.getElementsByTagNameNS('*','title')[1].childNodes[0].nodeValue,
+						iconUrl: 'images/logo128.png',
+						type: 'basic'
+					});
+				}else{
+					console.log("All updated !");
+				}
+	  
+			});
+		};
+		x.send();
+    }, 5000);
+  }
 });
--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ x.onload = function() {
     document.getElementById('body-con-id').innerHTML = "<canvas id='main-pie-chart' class='default-padding' width='100' height='50'></canvas>"
                                                               +"<div class='topic-data-div' id='td-data-header-id'></div>"
                                                               +"<div id='sub-data-grid'></div>" 
-                                                              +"<div class='footer' id='footer-id'>Data Source : Health Promotion Bureau, Sri Lanka</div>";
+                                                              +"<div class='footer' id='footer-id'>Data Source : Health Promotion Bureau, Sri Lanka, Google News</div>";
 
     var ctx = document.getElementById('main-pie-chart').getContext('2d');
 
@@ -46,30 +46,34 @@ x.onload = function() {
                                                                 +"</table>";
 
     document.getElementById('main-sum').innerHTML = "<td class='td-data td-tot' title='"+statisticalData.data.local_total_cases+" confirmed COVID-19 cases reported in Sri Lanka' >"+(Math.trunc(statisticalData.data.local_total_cases/1000))+"K</td> <td class='td-data td-rec' title='"+statisticalData.data.local_recovered+" COVID-19 cases recovered and discharged in Sri Lanka'>"+(Math.trunc(statisticalData.data.local_recovered/1000))+"K</td> <td class='td-data td-death' title='Total deaths due to COVID-19 reported in Sri Lanka'>"+statisticalData.data.local_deaths+"</td>";
-	document.getElementById('daily-sum').innerHTML = "<td class='td-data-header td-tot' title='Total confirmed COVID-19 cases reported in Sri Lanka toady'><span style='color: #ffffff7a;'>"+statisticalData.data.local_new_cases+"+</span></td>"
+	document.getElementById('daily-sum').innerHTML = "<td class='td-data-header td-tot' title='Total confirmed COVID-19 cases reported in Sri Lanka toady'><span style='color: #ffffff7a;'>+"+statisticalData.data.local_new_cases+"</span></td>"
 														+"<td class='td-data-header td-rec' title=''></td>"
-														+"<td class='td-data-header td-death' title='Total deaths due to COVID-19 reported in Sri Lanka today'><span style='color: #ffffff7a;'>"+statisticalData.data.local_new_deaths+"+</span></td>";
+														+"<td class='td-data-header td-death' title='Total deaths due to COVID-19 reported in Sri Lanka today'><span style='color: #ffffff7a;'>+"+statisticalData.data.local_new_deaths+"</span></td>";
 
     var i;
-    var subDataDiv = "";
-    
-    for (i = 0; i < statisticalData.data.hospital_data.length; i++) {
-      subDataDiv += "<div class='sub-data-div'>"
-              			+"<div style='padding-bottom: 6px; '>"+statisticalData.data.hospital_data[i].hospital.name+"</div>"
-              			+"<table style='width:100%; border-spacing: 0px' >"
-               			+" <tr>"
-                  		+"<td class='sub-data-txt' title='Total count of patients who are currently on treatment or observation on COVID-19 in "+statisticalData.data.hospital_data[i].hospital.name+"' >On Observation</td>"
-                  		+"<td class='sub-data-txt sub-data-test' title='Total count of patients who have been treated or observed on COVID-19 in "+statisticalData.data.hospital_data[i].hospital.name+"' >Observed</td>"
-                		+"</tr>"
-                		+"<tr>"
-                  		+"<td class='sub-data-num' title='Total count of patients who are currently on treatment or observation on COVID-19 in "+statisticalData.data.hospital_data[i].hospital.name+"'>"+statisticalData.data.hospital_data[i].treatment_total+"</td>"
-                  		+"<td class='sub-data-num sub-data-test' title='Total count of patients who have been treated or observed on COVID-19 in "+statisticalData.data.hospital_data[i].hospital.name+"' >"+statisticalData.data.hospital_data[i].cumulative_total+"</td>"
-		                +"</tr>"
-		              	+"</table>"
-		            	+"</div>";
-    }
+	var subDataDiv = "";
 
-    document.getElementById('sub-data-grid').innerHTML = subDataDiv;
+	chrome.storage.sync.get('latest_news', function(data) {
+		subDataDiv += "<div class='sub-news-div'>"
+								+"<div class='news-header'>Latest News</div>"
+								+"<div class='sub-news-txt'>"+data.latest_news+"</div>"
+						+"</div>"
+
+		document.getElementById('sub-data-grid').innerHTML = subDataDiv;
+	});
+
+	chrome.storage.sync.get('latest_news_link', function(data) {
+		var news_cont = document.getElementsByClassName("sub-news-div");
+		var news_cont_body = document.getElementsByClassName("sub-news-txt");
+
+		var a = document.createElement('a');
+		a.setAttribute('href',data.latest_news_link);
+		a.setAttribute('class','sub-news-txt');
+		a.setAttribute('target','_blank');
+		a.innerHTML = news_cont_body[0].innerText;
+		news_cont[0].replaceChild(a, news_cont_body[0]);
+
+	});
 
     document.getElementById('update-on').innerHTML = "Last Updated On: "+statisticalData.data.update_date_time;
 
